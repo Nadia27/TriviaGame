b5862d70e8ddff0ea4c9c31423e67cdcac0e783b
Nadia27 <jones.nadia.l@gmail.com>
2017-12-16 00:40:31 -0500

    created game home screen added new file and js pseudo code

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+//create an array of questions???
+//generate one question at a time on screen until time runs out or player answers
+//each question will be an timed event 
+//if player select right answer...show congrats
+		//after a few seconds display next quetion(without user input)
+//else player selects wrong answer
+		//tell player they selected wrong answer and display right answer
+		//display correct answer...after few seconds show next question
+	///timed out... 
+		//tell player times up
+		//display correct answer...after few seconds show next question(w/o user input)
+
+//Final screen...show number of correct/incorrect answers and option to restart game (w/o restarting page)
