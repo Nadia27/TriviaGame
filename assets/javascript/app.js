d1d55c4315d520af0067dab4fcb1a6fea957ee85
Nadia27 <jones.nadia.l@gmail.com>
2017-12-23 14:39:38 -0500

    correct/incorrect glitches fixed, timer fixed

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,7 +4,7 @@ var position = 0;
 var correct= 0; 
 var incorrect=0;
 
-//create an array of questions 
+//create an array of questions/////////////////////////////////////////////////////////////////////
 var trivia = [
 		
 		["The external JavaScript file must contain the '<'script'>' tag.", true, false, false], 
@@ -14,7 +14,70 @@ var trivia = [
 		["To round the number 7.25, to the nearest integer, Math.rnd(7.25).", true, false, false]
 ]
 
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+function doc(x) {
+	return document.getElementById(x); 
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+//Render questions////////////////////////////////////////////////////////////////////////////////
+function generateQuestion() {
+
+	var test = doc("triviaquest"); 
+	var question = trivia[y][0]; 
+	var choiceTrue = trivia[y][1]; 
+	var choiceFalse =trivia[y][2];
+	
+	test.innerHTML = "<h2>" + question + "</h2>";
+	"<br>"
+	test.innerHTML += "<input type ='radio' class='choices'  id= 'red' value='true' > " +choiceTrue+"<br>";
+	test.innerHTML += "<input type ='radio' class='choices' value='false'> " +choiceFalse+"<br><br"; 
+	test.innerHTML += "<button onclick='checkAnswer()'> Submit</button>";
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+//Check Answers////////////////////////////////////////////////////////////////////////////////////
+var y = 0;
+function checkAnswer() {
+
+	//var answer = trivia[position][3];
+ 
+	//check user input for right or wrong answer
+	
+	var userInput = document.getElementById("red").checked;
+	//alert(userInput);
+	//for( var i = 0; i < choices.length; i++) {
+	//	if(choices == true  ) {
+		 
+	if (userInput == trivia[y][3]) {
+		 clearInterval(intervalId);
+		 $("#quest-timer").html("<h3>" + "Correct" + "</h3>");
+          
+		 
+	}
+		else{
+			 clearInterval(intervalId);
+			
+        
+		}
+		y++;
+		setTimeout(stop, 1000 * 4);
+		 setTimeout(generateQuestion, 1000 * 5);
+		//console.log(correct);
+		}
+	//}
+	
+	generateQuestion(); 
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
 //Timed question will be 25 seconds per question
+
+
 var number = 25; 
 
 //This variable will hold interval ID 
@@ -23,6 +86,8 @@ var intervalID;
 function run () {
 	intervalId = setInterval(decrement, 1000); 
 }
+
+run();
 
 function decrement () {
 //decrease the number by 1 
@@ -33,60 +98,42 @@ function decrement () {
 
 	//when the number hits zero...
 	if(number === 0) {
+		//tell player times up and show answer after a few seconds generated new question
+
 		
 		stop(); 
 
 	//alert...change to restart to start new question and restart timer
-	alert("time up!"); 
+ $("#quest-timer").html("<h3>" + "Time Up" + "</h3>");
 	} 
 }
 
+
+
 function stop() {
 	clearInterval(intervalId); 
+	number =30;
+	run();
 
 }
-run(); 
+ 
 
 
 
 
 
 
-function doc(x) {
-	return document.getElementById(x); 
-}
 
-//Render questions
-function generateQuestion() {
 
-	var test = doc("triviaquest"); 
-	var question = trivia[position][0]; 
-	var choiceTrue = trivia[position][1]; 
-	var choiceFalse =trivia[position][2];
+
+
+
+
 	
-	test.innerHTML = "<h2>" + question + "</h2>";
-	"<br>"
-	test.innerHTML += "<input type ='radio' id='choices' value=true> " +choiceTrue+"<br>";
-	test.innerHTML += "<input type ='radio' id='choices' value= false> " +choiceFalse+"<br>"; 
-	test.innerHTML += "<button onclick='checkAnswer()'> Submit</button>";
-}
 
-function checkAnswer() {
-	//check user input for right or wrong answer
-	choices = doc("choices");
-	for( var i= 0; i < choices.length; i++) {
-		if(choices[i].checked) {
-			var choice = choices[i].value; 
-		}
-	}if (choice ==trivia[position][3]) {
-		correct++; 
-	}
-	position++; 
-	generateQuestion();
 
-}
 
-generateQuestion();
+
 
 
 
